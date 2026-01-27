a71f3269c30b9996b6ed3debfbcea5426a9d8c43
Informatica <informatica@alumno.local>
2026-01-27 14:31:43 -0600

    se hicieron cambios en el proyecto clase 26 de enero

---
+++ AuthServiceIN6BV/src/AuthServiceIN6BV.Application/Service/UuidGenerator.cs
@@ -0,0 +1,20 @@
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AuthService.Application.Services;
+
+public static class UuidGenerator
+{
+    
+    private static readonly string Alphabet = "123456789ABCDEFGHJKMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz";
+
+    public static string GenerateShortUUID()
+    {
+        using var rng = RandomNumberGenerator.Create();
+        var bytes = new byte[12];
+        rng.GetBytes(bytes);
+
+        
+    }
+}
