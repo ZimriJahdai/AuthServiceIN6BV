172f849c3fe302cebd98703021b97e90afb33d94
jhernandez2022 <jhernandez-2022010@kinal.edu.gt>
2026-02-03 17:21:50 -0600

    se hizo cambios en el proyecto clase 3 de febrero 2026

--- AuthServiceIN6BV/src/AuthServiceIN6BV.Persistence/obj/Debug/net8.0/AuthServiceIN6BV.Persistence.AssemblyInfo.cs
+++ AuthServiceIN6BV/src/AuthServiceIN6BV.Persistence/obj/Debug/net8.0/AuthServiceIN6BV.Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServiceIN6BV.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b2a009a13eb1b6cd03826d61c87f6e3d0f35f30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+524364bd4b1de0ea41ad628863a5240811fcac67")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServiceIN6BV.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServiceIN6BV.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
